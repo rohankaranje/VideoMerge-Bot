33182ed2187fce4e13ccb433f3a5706fc10e9a53
rohankaranje <149205870+rohankaranje@users.noreply.github.com>
2024-05-22 23:35:53 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,20 +12,20 @@ class Config(object):
     LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
     DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
     TIME_GAP = int(os.environ.get("TIME_GAP", 5))
-    MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 5))
+    MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 10))
     STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME")
     STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
     MONGODB_URI = os.environ.get("MONGODB_URI")
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1445283714))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1315497648))
 
     START_TEXT = """
 Hi Unkil, I am Video Merge Bot!
 I can Merge Multiple Videos in One Video. Video Formats should be same.
 
-Made by @AbirHasan2005
+Made by @rohankaranje
 """
-    CAPTION = "Video Merged by @{}\n\nMade by @AbirHasan2005"
+    CAPTION = "Video Merged by @{}\n\nMade by @rohankaranje"
     PROGRESS = """
 Percentage : {0}%
 Done: {1}
